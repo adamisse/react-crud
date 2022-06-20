4c655fb4a075b8198105cc1348c45e2e695a44b9
Matheus Bernardes <matheus.abmoreira@gmail.com>
2022-06-20 01:54:29 -0300

    Setup do componente app; Adição do CORS no Backend

--- crud-backend/src/server.js
+++ crud-backend/src/server.js
@@ -1,17 +1,10 @@
-const express = require('express');
-const toDosRoutes = require('./todos.routes');
-
+const express = require("express");
+const toDosRoutes = require("./todos.routes");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(toDosRoutes);
-
-//Select -> SQL
-//Read -> CRUD (Create, Read, Update, Delete)
-//GET -> Api
-
-app.get('/usuarios/', (req, res) =>{
-    return res.json("qualquer coisa");
-});
 
 app.listen(3030, () => console.log("Server tá on..."));
--- crud-frontend/src/App.js
+++ crud-frontend/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const arrayTodos = [
+  {name: "Testa", status: true},
+  {name: "Testandinho", status: false}
+]
+
+//Estudar essa atribuição de parâmetro objeto...
+const Todos = ({todos}) =>{
+  return(
+    <div className="todos">
+      {todos.map(todo => {
+        return(
+          <div className="todo"> 
+            {/* verifica se o status é true ou false*/}
+            <button className='checkbox' style={{backgroundColor: todo.status ? 'lightgreen' : 'White'}}></button>
+            <p>{todo.name}</p>
+            <button>
+              <AiOutlineEdit size={20} />
+            </button>
+            <button>
+              <AiOutlineDelete size={20} />
+            </button>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
 
 function App() {
+  async function getTodos(){
+    const response = await axios.get('http://localhost:5455');
+    console.log(response);
+  }
+
+  const [todos, setTodos] = useState([])//Estudar esse array?
+
+  //UseEffect irá chamar a função getTodos assim que o componente (app) for montado
+  useEffect(() => {
+    getTodos();
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="container">
+      <header className='header'>
+        <h1>Todo List</h1>
       </header>
-    </div>
+      
+      <Todos todos={arrayTodos} />
+      
+      <input className='inputName'></input>
+      <button className='newTaskButton'>+ New Task</button>
+    </section>
   );
 }
 
