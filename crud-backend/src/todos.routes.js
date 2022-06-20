3805de19b616b7ebda6244c12a69a487aad9d214
Matheus Bernardes <matheus.abmoreira@gmail.com>
2022-06-20 01:23:11 -0300

    Find by id implementado

--- crud-backend/src/todos.routes.js
+++ crud-backend/src/todos.routes.js
@@ -1,12 +1,12 @@
 // @ts-nocheck
-const express = require("express");
+const express = require('express');
 const toDosRoutes = express.Router();
 
-const { PrismaClient } = require("@prisma/client"); //Pacote escopado (garante que vem de uma organização confiável)
+const { PrismaClient } = require('@prisma/client'); //Pacote escopado (garante que vem de uma organização confiável)
 const prisma = new PrismaClient(); //Injetando o prisma via construtor
 
 //Create
-toDosRoutes.post("/todos", async (request, response) => {
+toDosRoutes.post('/todos', async (request, response) => {
 	const { name } = request.body;
 
 	const todo = await prisma.todo.create({
@@ -19,25 +19,25 @@ toDosRoutes.post("/todos", async (request, response) => {
 });
 
 //Read
-toDosRoutes.get("/todos", async (request, response) => {
+toDosRoutes.get('/todos', async (request, response) => {
 	const todos = await prisma.todo.findMany();
 
 	return response.status(200).json(todos);
 });
 
 //Update
-toDosRoutes.put("/todos", async (request, response) => {
+toDosRoutes.put('/todos', async (request, response) => {
 	const { id, name, status } = request.body;
 
 	if (!id) {
-		return response.status(400).json("Id é um parâmetro obrigatório!");
+		return response.status(400).json('Id é um parâmetro obrigatório!');
 	}
 
 	const todoAlreadyExists = await prisma.todo.findUnique({ where: { id } });
 
 	//caso não seja nulo
 	if (!todoAlreadyExists) {
-		return response.status(404).json("Todo não existe");
+		return response.status(404).json('Todo não existe');
 	}
 
 	//where serve para filtrar (nesse caso, pelo ID)
@@ -55,19 +55,19 @@ toDosRoutes.put("/todos", async (request, response) => {
 });
 
 //Delete
-toDosRoutes.delete("/todos/:id", async (request, response) => {
+toDosRoutes.delete('/todos/:id', async (request, response) => {
 	const { id } = request.params;
 	const intId = parseInt(id);
 
 	if (!intId) {
-		return response.status(400).json("Id é um parâmetro obrigatório!");
+		return response.status(400).json('Id é um parâmetro obrigatório!');
 	}
 
 	const todoAlreadyExists = await prisma.todo.findUnique({ where: { id: intId } });
 
 	//caso não seja nulo
 	if (!todoAlreadyExists) {
-		return response.status(404).json("Todo não existe");
+		return response.status(404).json('Todo não existe');
 	}
 
 	await prisma.todo.delete({
@@ -76,5 +76,20 @@ toDosRoutes.delete("/todos/:id", async (request, response) => {
 
 	return response.status(200).send();
 });
+
+toDosRoutes.get('/todos/:id', async (request, response) =>{
+    const { id } = request.params;
+    const intId = parseInt(id)
+
+    if (!intId) {
+		return response.status(400).json('Id é um parâmetro obrigatório!');
+	}
+
+    const todo = await prisma.todo.findUnique({
+		where: { id: intId },
+	});
+
+	return response.status(200).json(todo);
+})
 
 module.exports = toDosRoutes;
